a0f36490679eab5e29e9b2ff8857334b5f43e80c
Hisham <hishamsaeed2003@gmail.com>
2023-07-16 22:44:14 +0530

    deploy ready2

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9ml!eghah!__*jh3#l6!p*&cg#9=4)af=9q(z*c*yx9$2e6sq@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-16-170-212-230.eu-north-1.compute.amazonaws.com','127.0.0.1']
 
 
 # Application definition
