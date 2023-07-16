a1c4850f14e32ba2e7ab370ad07247c84d63f2dd
Hisham <hishamsaeed2003@gmail.com>
2023-07-16 23:23:44 +0530

    deploy ready4

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,13 @@
+env/scripts/activate
 
-python manage.py collectstatic
+# Install required packages
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic 
+
+# Apply database migrations
+python manage.py migrate
+
+# Start the Django development server or your preferred deployment method
+python manage.py runserver
